50f22ac4457d52f3e228f1b97a372158c44ed632
katiepru <katiepru@nbcs.rutgers.edu>
2013-10-06 13:03:40 -0400

    starting indexer project

---
+++ indexer/indexer.c
@@ -0,0 +1,17 @@
+#include <indexer.h>
+
+int main(int argc, char *argv[])
+{
+    //Check that we have the right number of args
+    if(argc < 3)
+    {
+        fprintf(stderr, "Please provide the output file and directory to index\n");
+        return 1;
+    }
+    if(argc > 3)
+    {
+        fprintf(stderr, "Please only provide the output file and directory to index\n");
+        return 1;
+    }
+    return 0;
+}
---
+++ indexer/indexer.h
@@ -0,0 +1,7 @@
+#ifndef _INDEXER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _INDEXER_H 1
+#endif
