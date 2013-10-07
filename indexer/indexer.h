aeb6012ef7ad81833d71358cfd2501a55a906f27
katiepru <katiepru@nbcs.rutgers.edu>
2013-10-06 21:05:50 -0400

    adding termnode struct

--- indexer/indexer.h
+++ indexer/indexer.h
@@ -3,5 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Struct definitions
+typedef struct TermNode {
+    char *term;
+    struct TermNode *next;
+    //FIXME: Add struct that contains file info
+} TermNode;
+
 #define _INDEXER_H 1
 #endif
