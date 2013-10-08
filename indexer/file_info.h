7de7ed70ae810f668949d5161b31b30dd8982632
katiepru <katiepru@nbcs.rutgers.edu>
2013-10-07 23:40:28 -0400

    removing stupid

--- indexer/file_info.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FILE_INFO_H
-
-//Struct definition
-typedef struct FileInfo {
-    //FIXME: Add struct that contains file info
-} FileInfo;
-
-#define _FILE_INFO_H 1
-#endif
--- indexer/indexer.h
+++ indexer/indexer.h
@@ -2,13 +2,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "file_info.h"
 
 //Struct definitions
 typedef struct TermNode {
     char *term;
     struct TermNode *next;
-    FileInfo *file_info;
 } TermNode;
 
 #define _INDEXER_H 1
