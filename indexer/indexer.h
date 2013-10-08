252c90b8ba8036b4bdcf1c2041e3a6348aa731fa
katiepru <katiepru@nbcs.rutgers.edu>
2013-10-07 23:41:48 -0400

    using LL

--- indexer/indexer.h
+++ indexer/indexer.h
@@ -2,11 +2,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "ll.h"
 
 //Struct definitions
 typedef struct TermNode {
     char *term;
     struct TermNode *next;
+    LLNode *files;
 } TermNode;
 
 #define _INDEXER_H 1
