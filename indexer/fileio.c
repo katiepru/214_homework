b38ea4015f0fe444c664f0205bcade3a32a6645f
Josh Matthews <josh@jmatthews.us>
2013-10-10 23:34:18 -0400

    Adding recursive call for fileio.

--- indexer/fileio.c
+++ indexer/fileio.c
@@ -3,6 +3,7 @@
 void for_file(char* dirname, void* func, void* arg)
 {
     struct dirent *entry;
+    char path[1024];
     DIR* dir;
 
     /* safety check, need non-null pointers */
@@ -18,15 +19,18 @@ void for_file(char* dirname, void* func, void* arg)
 
     do
     {
+        /* if it's a directory, recurse */
         if (entry->d_type == DT_DIR)
         {
-            char path[1024];
             int len = snprintf(path, sizeof(path)-1, "%s/%s", dirname, entry->d_name);
             path[len] = 0;
             if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+            {
                 continue;
-            /* recursive call here */
+            }
+            for_file(path, func, arg);
         }
+        /* if its a regular file, call the function on it */
         else
         {
             /* call function here */
