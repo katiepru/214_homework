03fe3bb072ee73b65f58ae4dc6c48cdc1d0db42e
katiepru <katiepru@nbcs.rutgers.edu>
2013-10-15 10:50:34 -0400

    adding some linked list stuff

--- indexer/ll.c
+++ indexer/ll.c
@@ -1,6 +1,28 @@
 #include "ll.h"
 
-LLNode *create_llnode(char *file_name)
+/*
+ * Allocates a list
+ */
+LL *create_list()
+{
+    LL *list = malloc(sizeof(LL));
+
+    if(list == NULL)
+    {
+        fprintf(stderr, "Malloc failed.\n");
+        exit(1);
+    }
+
+    list->head = NULL;
+    list->curr = NULL;
+
+    return list;
+}
+
+/*
+ * Allocates a node
+ */
+LLNode *create_llnode(char *file_name, LL *list)
 {
     LLNode *n = malloc(sizeof(LLNode));
     if(n == NULL)
@@ -10,10 +32,24 @@ LLNode *create_llnode(char *file_name)
     }
 
     n->file_name = file_name;
+    n->list = list;
 
     return n;
 }
 
+/*
+ * Frees a list
+ * Assumes list is not NULL
+ */
+void destroy_list(LL* list)
+{
+    destroy_llnode(list->head);
+    free(list);
+}
+
+/*
+ * Frees a node and all of it's children
+ */
 void destroy_llnode(LLNode *n)
 {
     if(n == NULL)
--- indexer/ll.h
+++ indexer/ll.h
@@ -5,11 +5,26 @@
 #include <stdio.h>
 
 //Struct definition
-typedef struct LLNode {
+typedef struct LLNode LLNode;
+typedef struct LL LL;
+
+struct LLNode {
     char *file_name;
     int count;
-    struct LLNode *next;
-} LLNode;
+    LLNode *next;
+    LL *list;
+};
+
+struct LL {
+    LLNode *head;
+    LLNode *curr;
+};
+
+//Function declarations
+LL *create_list();
+LLNode *create_llnode(char *, LL *);
+void destroy_list(LL *);
+void destroy_llnode(LLNode *);
 
 #define _LL_H 1
 #endif
