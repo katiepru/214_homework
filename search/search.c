05afbc6e1b27e6b93bf648848be9a6d870bd1699
katiepru <katiepru@nbcs.rutgers.edu>
2013-11-02 17:32:12 -0400

    working on preprocessing

--- search/search.c
+++ search/search.c
@@ -2,6 +2,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "structs/trie.h"
+#include "structs/file-node.h"
+#include "structs/sorted-list.h"
+
+//Function Declarations
+Trie *preprocess_file(FILE *);
 
 #define _SEARCH_H 1
 #endif
--- search/search.h
+++ search/search.h
@@ -21,4 +21,76 @@ int main(int argc, char **argv)
         return 2;
     }
 
+    Trie *file_contents = preprocess_file(input);
+
+}
+
+/*
+ * Preprocess file to make queries fast
+ */
+Trie *preprocess_file(FILE *input)
+{
+    int fscanf_result = 1;
+    char term[1000];
+    char file[1000];
+    Trie *trie = create_trie(destroy_data_in_sorted_list, insert_data_into_sorted_list);
+    TrieNode *curr_node;
+    FileNode *fnode;
+
+    while(fscanf_result != EOF)
+    {
+        //First, get term
+        fscanf_result = fscanf(input, "%s %s", NULL, term);
+        if(fscanf_result < 2)
+        {
+            continue;
+        }
+
+        //Next, add the term to the trie
+        insert_word(term, NULL, trie);
+        curr_node = find_word(term, trie);
+
+        //Term is in trie, now add files
+        do {
+            fscanf_result = fscanf(input, "%s %d", file, NULL);
+            fnode = creat_filenode(file);
+            trie->insert_data(curr_node, (void *) fnode);
+        } while(fscanf_result > 1);
+
+    }
+
+
+}
+
+/*
+ * Destroys a sorted list
+ */
+void destroy_data_in_sorted_list(void *data);
+{
+    SortedListPtr list = (SortedListPtr) data;
+    SLDestroy(list);
+}
+
+/*
+ * Inserts our data into a sorted list
+ */
+void insert_data_into_sorted_list(TrieNode *node, void *data)
+{
+    if(node->data == NULL)
+    {
+        SLCreate(compare_file_nodes);
+    }
+
+    SLInsert((SortedListPtr) node->data, data);
+}
+
+/*
+ * Compares file nodes alphabetically by file name
+ */
+int compare_file_nodes(void *p1, void *p2)
+{
+    FileNode *f1 = (FileNode *) p1;
+    FileNode *f2 = (FileNode *) p2;
+
+    return strcmp(f1->file_name, f2->file_name);
 }
