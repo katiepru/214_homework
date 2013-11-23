43a3df4f5aad8a8eb903d4a62dda0c577d690ffe
katiepru <katiepru@nbcs.rutgers.edu>
2013-11-23 16:19:52 -0500

    enqueueing orders

--- book_order/book-order.c
+++ book_order/book-order.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     customer_trie = build_customer_trie(argv[1]);
 
     //Enqueue orders
-    enqueue_orders(argv[2]);
+    enqueue_orders(argv[2], order_trie);
 
     //Now, spin up helper threads to process orders
 
@@ -49,13 +49,96 @@ Trie *build_category_trie(char **args, int start, int argc)
 
     for(i = start; i < argc; i++)
     {
-        q = queue_init(destroy_category_queue_node);
+        q = queue_init(destroy_order_info);
         insert_word(args[i], q, t);
     }
 
     return t;
 }
 
-void destroy_category_queue_node(void *n)
+void enqueue_orders(const char *filename, Trie *category_trie)
 {
+    char *book_name;
+    char *book_category;
+    int book_price;
+    int customer_id;
+
+    OrderInfo *o;
+    TrieNode *n;
+
+    char buffer[1024];
+    FILE *f = fopen(filename, "r");
+
+    if(f == NULL)
+    {
+        fprintf(stderr, "Failed to open file %s.\n", filename);
+    }
+
+    while(fgets(buffer, sizeof(buffer), f) != NULL)
+    {
+        book_name = strtok(buffer, "\"|");
+        book_price = (int) (100*atol(strtok(NULL, "|")));
+        customer_id = atoi(strtok(NULL, "|"));
+        book_category = strtok(NULL, "\n");
+
+        //Create order stuct
+        o = create_order(book_name, book_category, book_price,
+                        customer_id);
+
+        //Enqueue struct into correct queue
+        n = find_word(book_category, category_trie);
+        category_trie->insert_data(n, (void *)o);
+    }
+}
+
+//Helpers
+
+void insert_into_queue(TrieNode *n, void *data)
+{
+    SynchQueue *q = (SynchQueue *) n->data;
+    enqueue(q, data);
+}
+
+void destroy_queue(void *data)
+{
+    SynchQueue *q = (SynchQueue *) data;
+    queue_destroy(q);
+}
+
+//Functions to handle order info structs
+
+OrderInfo *create_order(char *name, char *category,
+                       int price, int customer_id)
+{
+    OrderInfo *o = malloc(sizeof(OrderInfo));
+
+    if(o == NULL)
+    {
+        fprintf(stderr, "Malloc failed\n");
+        return NULL;
+    }
+
+    o->book_name = malloc(strlen(name) + 1);
+    strcpy(o->book_name, name);
+
+    o->category = malloc(strlen(category) + 1);
+    strcpy(o->category, category);
+
+    o->price = price;
+    o->cid = customer_id;
+
+    return o;
+
+}
+
+void destroy_order_info(void *o)
+{
+    if(o == NULL) return;
+
+    OrderInfo *order = (OrderInfo *) o;
+
+    free(order->book_name);
+    free(order->category);
+
+    free(order);
 }
--- book_order/book-order.h
+++ book_order/book-order.h
@@ -8,15 +8,28 @@
 #include "structs/trie.h"
 #include "structs/synch-queue.h"
 
+//Struct definitions
+typedef struct OrderInfo OrderInfo;
+struct OrderInfo {
+    char *book_name;
+    char *category;
+    int cid;
+    int price;
+};
+
 //Function declarations
 Trie *build_category_trie(char **, int, int);
 Trie *build_customer_trie(const char *);
-void enqueue_orders(const char *);
+void enqueue_orders(const char *, Trie *);
 
 //Helper functions
 void insert_into_queue(TrieNode *, void *);
 void destroy_queue(void *);
-void destroy_category_queue_node(void *);
+
+//Struct management fuctions
+OrderInfo *create_order(char *, char *, int, int);
+void destroy_order_info(void *);
+
 
 #define _BOOK_ORDER_H 1
 #endif
